a3c8f154b10cd7f4f9556092a286c2f5b7ef3fc1
HBedwell24 <32681500+HBedwell24@users.noreply.github.com>
2018-11-07 11:30:57 -0500

    Update sketch.ino
    Added the missing setup and loop functions, fixed the bridge library conflicts, and set pin 8 to input so that the PIR motion sensor can successfully update the other components in the project

--- sketch.ino
+++ sketch.ino
@@ -6,24 +6,35 @@ String filename;
 int pir_pin = 8;
 String path = "/mnt/sda1/";
 
-Bridge.begin();
+void setup() {
+  Bridge.begin();
+  pinMode(pir_pin,INPUT);
+}
 
-if (digitalRead(pir_pin) == true) {
+void loop() {
+  if (digitalRead(pir_pin) == true) {
+    filename = "";
+    picture.runShellCommand("date +%s");
+    while(picture.running());
+   
+    while (picture.available()>0) {
+      char c = picture.read();
+      filename += c;
+    } 
+  
+    filename.trim();
+    filename += ".png";
 
-	filename = "";
-	picture.runShellCommand("date +%s");
-	while(picture.running());
-	 
-	while (picture.available()>0) {
-		char c = picture.read();
-		filename += c;
-	} 
-	
-	filename.trim();
-	filename += ".png";
-	
-picture.runShellCommand("fswebcam " + path + filename + " -r 1280x720");
-while(picture.running());
-picture.runShellCommand("python " + path + "upload_picture.py " + path + filename);
-while(picture.running());
+    // Save the picture locally on a microSD located within the Arduino Yun
+    picture.runShellCommand("fswebcam " + path + filename + " -r 1280x720");
+    while(picture.running());
+    Serial.println("File successfully saved locally!");
+
+    // Upload to Amazon S3
+    picture.runShellCommand("python " + path + " " + path + filename);
+    while(picture.running());
+}
+}
+
+
 	
